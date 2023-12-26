703975685dac845ea8df1b0186780d8b08054fed
Tanjim Abubokor <59773799+tanjim750@users.noreply.github.com>
2023-12-26 23:12:07 +0600

    Update urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,7 @@
-"""movies URL Configuration
+"""wafid_autopay URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,9 +15,21 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from movie_app import views
+
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name="home")
+    path("pay-now/",views.PayNow.as_view(), name='pay_now'),
+    # path("pay-with-selenium/",views.PayWithSelenium.as_view(), name='pay-with-selenium'),
+    path("login/",views.LoginView.as_view(), name='login'),
+    path("",views.HomeView.as_view(), name='home'),
+    path("add-link",views.AddLink.as_view(), name='add_link'),
+    path("remove-link",views.RemoveLink.as_view(), name='remove_link'),
+    path("update-link",views.UpdateLink.as_view(), name='update_link'),
+    path("add-to-tab",views.AddToTab.as_view(), name='add_to_tab'),
+    path("booking",views.BookingView.as_view(), name='booking'),
+    path("booking-links",views.BookingLinkView.as_view(), name='booking_links'),
+    path("book-again",views.BookAgainView.as_view(), name='book_again'),
+    path("logout",views.LogoutView.as_view(), name='logout'),
 ]
